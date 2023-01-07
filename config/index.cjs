27c03d81da58a7afef6445b225aeaebec300ab53
130633 <122135600+130633@users.noreply.github.com>
2023-01-07 11:54:05 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -19,13 +19,13 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '宝贝',
+      name: '小刘',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: '',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'Wa4g6JOrZh2ee62hxEUHaXRGfbLPpuBp-A_Sv7a1174',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
-      horoscopeDate: '12-27',
+      horoscopeDate: '4-3',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
